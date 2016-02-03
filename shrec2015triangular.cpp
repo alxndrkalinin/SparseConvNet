a6d297468d99df60042390efa48c2b2dbf2a8f55
alxndrkalinin <alxndrkalinin@gmail.com>
2016-02-03 15:15:00 -0500

    feat(shrec2015triangular): add VGG and NiN -like nets

--- shrec2015triangular.cpp
+++ shrec2015triangular.cpp
@@ -19,23 +19,40 @@ DeepC2Triangular::DeepC2Triangular(int dimension, int l, int k,
 //  for (int i = 0; i <= l; i++)
 //    addLeNetLayerMP((i + 1) * k, (i == l) ? 2 : 2, 1, (i < l) ? 3 : 1,
 //                              (i < l) ? 2 : 1, fn, p * i * 1.0f / l);
+  
+  // VGG-like
   // addTriangularLeNetLayerMP(nFeatures, filterSize, filterStride, poolSize, poolStride, activationFn, dropout, minActiveInputs);
-  addLeNetLayerMP(k, 3, 1, 1, 1, fn, 0); // no pooling
-  addLeNetLayerMP(2 * k, 3, 1, 2, 2, fn, p * 1.0f / l);
-  addLeNetLayerMP(3 * k, 3, 1, 1, 1, fn, p * 2.0f / l); // no pooling
-  addLeNetLayerMP(4 * k, 3, 1, 2, 2, fn, p * 3.0f / l);
-  addLeNetLayerMP(5 * k, 3, 1, 1, 1, fn, p * 4.0f / l); // no pooling
-  addLeNetLayerMP(6 * k, 3, 1, 1, 1, fn, p * 5.0f / l); // no pooling
-  addLeNetLayerMP(7 * k, 3, 1, 2, 2, fn, p * 6.0f / l);
-  addLeNetLayerMP(8 * k, 3, 1, 1, 1, fn, p * 7.0f / l); // no pooling
-  addLeNetLayerMP(9 * k, 3, 1, 1, 1, fn, p * 8.0f / l); // no pooling
-  addLeNetLayerMP(10 * k, 3, 1, 2, 2, fn, p * 9.0f / l);
-  addLeNetLayerMP(11 * k, 3, 1, 1, 1, fn, p * 10.0f / l); // no pooling
-  addLeNetLayerMP(12 * k, 3, 1, 1, 1, fn, p * 11.0f / l); // no pooling
-  addLeNetLayerMP(13 * k, 1, 1, 1, 1, fn, p * 12.0f / l);
-  addLeNetLayerMP(14 * k, 1, 1, 1, 1, fn, p * 13.0f / l);
+//  addLeNetLayerMP(k, 3, 1, 1, 1, fn, 0); // no pooling
+//  addLeNetLayerMP(k, 3, 1, 2, 1, fn, p * 1.0f / l);
+//  addLeNetLayerMP(2 * k, 3, 1, 1, 1, fn, p * 2.0f / l); // no pooling
+//  addLeNetLayerMP(2 * k, 3, 1, 2, 2, fn, p * 3.0f / l);
+//  addLeNetLayerMP(3 * k, 3, 1, 1, 1, fn, p * 4.0f / l); // no pooling
+//  addLeNetLayerMP(3 * k, 3, 1, 1, 1, fn, p * 5.0f / l); // no pooling
+//  addLeNetLayerMP(3 * k, 3, 1, 2, 2, fn, p * 6.0f / l);
+//  addLeNetLayerMP(4 * k, 3, 1, 1, 1, fn, p * 7.0f / l); // no pooling
+//  addLeNetLayerMP(4 * k, 3, 1, 1, 1, fn, p * 8.0f / l); // no pooling
+//  addLeNetLayerMP(4 * k, 3, 1, 2, 2, fn, p * 9.0f / l);
+//  addLeNetLayerMP(4 * k, 3, 1, 1, 1, fn, p * 10.0f / l); // no pooling
+//  addLeNetLayerMP(4 * k, 3, 1, 1, 1, fn, p * 11.0f / l); // no pooling
+//  addLeNetLayerMP(4 * k, 1, 1, 2, 2, fn, p * 12.0f / l);
+//  addLeNetLayerMP(24 * k, 1, 1, 1, 1, fn, 0.5f);
+//  addLeNetLayerMP(24 * k, 1, 1, 1, 1, fn, 0.5f);
 //  addLeNetLayerMP(15 * k, 1, 1, 1, 1, fn, p * 13.0f / l);
 
+  // NiN-like
+  addLeNetLayerMP(192, 5, 1, 1, 1, fn, 0); // no pooling
+  addLeNetLayerMP(160, 1, 1, 1, 1, fn, 0); // no pooling
+  addLeNetLayerMP(96, 1, 1, 3, 2, fn, 0.5); // max-pooling + dropout
+  
+  addLeNetLayerMP(192, 5, 1, 1, 1, fn, 0); // no pooling
+  addLeNetLayerMP(192, 1, 1, 1, 1, fn, 0); // no pooling
+  addLeNetLayerMP(192, 1, 1, 3, 2, fn, 0.5); // max-pooling + dropout
+  
+  addLeNetLayerMP(192, 3, 1, 1, 1, fn, 0); // no pooling
+  addLeNetLayerMP(192, 1, 1, 1, 1, fn, 0); // no pooling
+  addLeNetLayerMP(10, 1, 1, 8, 1, fn, 0); // max-pooling
+
+  // Softmax output
   addSoftmaxLayer();
 }
 
@@ -45,22 +62,24 @@ int main(int lenArgs, char *args[]) {
   if (lenArgs > 1)
     int fold = atoi(args[1]);
   std::cout << "Fold: " << fold << std::endl;
-  SpatiallySparseDataset trainSet = SHREC2015TrainSet(80, 6, fold);
+  SpatiallySparseDataset trainSet = SHREC2015TrainSet(60, 10, fold);
   trainSet.summary();
-  trainSet.repeatSamples(10);
-  SpatiallySparseDataset testSet = SHREC2015TestSet(80, 6, fold);
+  trainSet.repeatSamples(15);
+  trainSet.summary();
+  SpatiallySparseDataset testSet = SHREC2015TestSet(60, 10, fold);
   testSet.summary();
 
-  DeepC2Triangular cnn(3, 6, 32, VLEAKYRELU, trainSet.nFeatures,
+  DeepC2Triangular cnn(3, 10, 60, VLEAKYRELU, trainSet.nFeatures,
                        trainSet.nClasses, 0.0f, cudaDevice);
+  
   if (epoch > 0)
     cnn.loadWeights(baseName, epoch);
-  for (epoch++; epoch <= 100 * 2; epoch++) {
+  for (epoch++; epoch <= 100 * 5; epoch++) {
     std::cout << "epoch:" << epoch << ": " << std::flush;
-    cnn.processDataset(trainSet, batchSize, 0.003 * exp(-0.05 / 2 * epoch));
-    if (epoch % 20 == 0) {
-      cnn.saveWeights(baseName, epoch);
-      cnn.processDatasetRepeatTest(testSet, batchSize, 3);
+    cnn.processDataset(trainSet, batchSize, 0.003 * exp(-0.01 * epoch));
+    if (epoch % 1 == 0) {
+//      cnn.saveWeights(baseName, epoch);
+      cnn.processDatasetRepeatTest(testSet, batchSize, 1);
     }
   }
 }
